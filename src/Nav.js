b74c8b55325871d7390a68d66e737c5ec1cf747a
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-12-08 00:02:08 +0530

    Initial Commit

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React,{useContext} from 'react';
+import { MovieContext } from './MovieContext';
+
+export default function Nav() {
+    const [movies,setMovies]= useContext(MovieContext);
+    return (
+        <div>
+            <h3> Dev Ed</h3>
+            <p> List of Movies: {movies.length}</p>
+        </div>
+    )
+}
